2fb8a46d7108215f5ce36fa3967f8d357b6983e2
jpablopachar <jpablopachar1993@gmail.com>
2025-04-30 19:41:33 -0500

    feat: agregar configuración del cliente de Supabase

---
+++ src/supabase/supabase.config.ts
@@ -0,0 +1,13 @@
+import { createClient } from '@supabase/supabase-js'
+
+/**
+ * Configuración del cliente de Supabase
+ * @constant {object} supabase - Instancia del cliente de Supabase.
+ * @description
+ * Este objeto contiene la configuración para el cliente de Supabase.
+ * Se crea utilizando la función `createClient` de la biblioteca de Supabase.
+ */
+export const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
+)
