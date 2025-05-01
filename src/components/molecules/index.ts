542a7be0e395b84351b386f37b915615b72858ca
jpablopachar <jpablopachar1993@gmail.com>
2025-05-01 09:43:04 -0500

    feat(components): Agregar exportaciones de componentes en index.ts

---
+++ src/components/molecules/index.ts
@@ -0,0 +1,53 @@
+export * from './AnimationLottie'
+
+export * from './BlockingPage'
+
+export * from './BlockingPageStyles'
+
+export * from './BtnCircular'
+
+export * from './BtnCircularStyles'
+
+export * from './BtnDropdown'
+
+export * from './BtnDropdownStyles'
+
+export * from './BtnFilter'
+
+export * from './BtnFilterStyles'
+
+export * from './BtnSave'
+
+export * from './BtnSaveStyles'
+
+export * from './CardCompanyData'
+
+export * from './CardCompanyDataStyles'
+
+export * from './GenericList'
+
+export * from './GenericListStyles'
+
+export * from './InputListSearchEngine'
+
+export * from './InputListSearchEngineStyles'
+
+export * from './ItemDropdown'
+
+export * from './ItemDropdownStyles'
+
+export * from './MenuDropdownList'
+
+export * from './MenuDropdownListStyles'
+
+export * from './Message'
+
+export * from './MessageStyles'
+
+export * from './ModulesListStyles'
+
+export * from './SpinnerLoaderStyles'
+
+export * from './SpinnerStyles'
+
+export * from './animatedFunds'
