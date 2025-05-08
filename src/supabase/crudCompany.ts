fd450a560073dd0c378302bb15280a9481e0a737
jpablopachar <jpablopachar1993@gmail.com>
2025-05-08 17:42:56 -0500

    feat(supabase): Agregar funciones para insertar y obtener registros de kardex

---
+++ src/supabase/crudCompany.ts
@@ -0,0 +1,87 @@
+/* eslint-disable no-console */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import { SweetAlertOptions } from 'sweetalert2'
+
+import { supabase } from './supabase.config'
+
+import { showAlert } from '@/utils'
+
+/**
+ * Obtiene la información de la compañía asignada a un usuario
+ * específico mediante una función remota (RPC) en Supabase.
+ *
+ * @param userId - El identificador único del usuario para el cual se desea obtener la compañía.
+ * @returns Una promesa que resuelve con los datos de la compañía
+ * si la operación es exitosa, o `null` si ocurre un error.
+ *
+ * @remarks
+ * - Muestra una alerta utilizando `showAlert` en caso de error.
+ * - Utiliza la función remota `showCompaniesAssignments` en Supabase.
+ * - Registra en consola el proceso de obtención y cualquier error encontrado.
+ */
+export const getCompany = async (userId: string): Promise<any> => {
+  console.log('Obteniendo compañía con ID de usuario: ', userId)
+
+  const { data, error } = await supabase
+    .rpc('showCompaniesAssignments', { _UserId: userId })
+    .maybeSingle()
+
+  if (error) {
+    console.error('Error al obtener la compañía: ', error)
+
+    const options: SweetAlertOptions = {
+      icon: 'error',
+      title: 'Oops...',
+      text: `Error al obtener la compañía: ${error.message}`,
+      footer: '<a href="">error</a>',
+    }
+
+    showAlert(options)
+
+    return null
+  }
+
+  console.log('Compañía obtenida: ', data)
+
+  return data
+}
+
+/**
+ * Cuenta el número de usuarios asociados a una compañía
+ * específica utilizando una función RPC de Supabase.
+ *
+ * @param companyId - El identificador único de la compañía para
+ * la cual se desea contar los usuarios.
+ * @returns Una promesa que resuelve con el número de usuarios
+ * asociados a la compañía, o `null` si ocurre un error.
+ *
+ * @remarks
+ * Si ocurre un error durante la consulta, se muestra una alerta al usuario y se retorna `null`.
+ */
+export const countUsersByCompany = async (companyId: string): Promise<any> => {
+  console.log('Contando usuarios por compañía con ID: ', companyId)
+
+  const { data, error } = await supabase
+    .rpc('countUsersByCompany', { _CompanyId: companyId })
+    .maybeSingle()
+
+  if (error) {
+    console.error('Error al contar usuarios por compañía: ', error)
+
+    const options: SweetAlertOptions = {
+      icon: 'error',
+      title: 'Oops...',
+      text: `Error al contar usuarios por compañía: ${error.message}`,
+      footer: '<a href="">error</a>',
+    }
+
+    showAlert(options)
+
+    return null
+  }
+
+  console.log('Número de usuarios por compañía: ', data)
+
+  return data
+}
---
+++ src/supabase/crudKardex.ts
@@ -0,0 +1,122 @@
+/* eslint-disable no-console */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import { SweetAlertOptions } from 'sweetalert2'
+
+import { supabase } from './supabase.config'
+
+import { showAlert } from '@/utils'
+
+/**
+ * Inserta un registro en la tabla 'kardex' utilizando Supabase.
+ *
+ * @async
+ * @function
+ * @param {any} kardex - Objeto que contiene los datos del kardex a insertar.
+ * @returns {Promise<void>} Una promesa que se resuelve cuando la operación ha finalizado.
+ *
+ * @description
+ * Esta función intenta insertar un nuevo registro en la tabla 'kardex' de la base de datos.
+ * Si ocurre un error durante la inserción, muestra una alerta con
+ * el mensaje de error utilizando SweetAlert.
+ * También imprime mensajes en la consola para depuración.
+ */
+export const insertKardex = async (kardex: any): Promise<void> => {
+  console.log('Insertando kardex: ', kardex)
+
+  const { error } = await supabase.from('kardex').insert(kardex)
+
+  if (error) {
+    console.error('Error al insertar el kardex: ', error)
+
+    const options: SweetAlertOptions = {
+      icon: 'error',
+      title: 'Oops...',
+      text: `Error al insertar el kardex: ${error.message}`,
+      footer: '<a href="">error</a>',
+    }
+
+    showAlert(options)
+  }
+
+  console.log('El kardex fue insertado correctamente: ')
+}
+
+/**
+ * Obtiene el kardex de una compañía a partir de su ID utilizando una función RPC de Supabase.
+ *
+ * @param id - El identificador único de la compañía cuyo kardex se desea obtener.
+ * @returns Una promesa que resuelve con los datos del kardex o `null` si ocurre un error.
+ *
+ * @remarks
+ * - Muestra una alerta utilizando `showAlert` si ocurre un error durante la obtención de datos.
+ * - Los resultados se ordenan de forma descendente por el campo `id`.
+ */
+export const getKardex = async (id: string): Promise<any> => {
+  console.log('Obteniendo kardex con ID: ', id)
+
+  const { data, error } = await supabase
+    .rpc('getKardexCompany', { _CompanyId: id })
+    .order('id', { ascending: false })
+
+  if (error) {
+    console.error('Error al obtener el kardex: ', error)
+
+    const options: SweetAlertOptions = {
+      icon: 'error',
+      title: 'Oops...',
+      text: `Error al obtener el kardex: ${error.message}`,
+      footer: '<a href="">error</a>',
+    }
+
+    showAlert(options)
+
+    return null
+  }
+
+  console.log('Kardex obtenido: ', data)
+
+  return data
+}
+
+/**
+ * Busca registros de kardex en la base de datos utilizando una función RPC de Supabase.
+ *
+ * @param kardex - Objeto que contiene los parámetros de búsqueda,
+ * incluyendo el ID de la compañía (`CompanyId`) y el término de búsqueda (`search`).
+ * @returns Una promesa que resuelve con los datos encontrados o `null` si ocurre un error.
+ *
+ * @remarks
+ * - Muestra una alerta utilizando `showAlert` en caso de error durante la búsqueda.
+ * - Los resultados se ordenan de forma descendente por el campo `id`.
+ * - Utiliza la función RPC `searchKardexCompany` en Supabase.
+ */
+export const searchKardex = async (kardex: any): Promise<any> => {
+  console.log('Buscando kardex: ', kardex)
+
+  const { data, error } = await supabase
+    .rpc('searchKardexCompany', {
+      _idCompany: kardex.CompanyId,
+      search: kardex.search,
+    })
+    .order('id', { ascending: false })
+
+  if (error) {
+    console.error('Error al buscar el kardex: ', error)
+
+    const options: SweetAlertOptions = {
+      icon: 'error',
+      title: 'Oops...',
+      text: `Error al buscar el kardex: ${error.message}`,
+      footer: '<a href="">error</a>',
+    }
+
+    showAlert(options)
+
+    return null
+  }
+
+  console.log('Kardex encontrado: ', data)
+
+  return data
+}
